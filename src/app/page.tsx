fa662a92b34ccac6a446d51881fedba85035c2eb
ShanmugaSundari10 <148695048+ShanmugaSundari10@users.noreply.github.com>
2025-11-04 17:08:57 +0530

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,27 @@
+import AboutMe from "./components/home/about-me"
+import Contact from "./components/home/contact"
+import EducationSkills from "./components/home/education-skills"
+import ExperienceSec from "./components/home/experience-sec"
+import HeroSection from "./components/home/hero-section"
+import ContactBar from "./components/home/hero-section/contact-bar"
+import LatestWork from "./components/home/latest-work"
+import SoftSkills from "./components/home/softskill"
+
+const page = () => {
+  return (
+    <>
+      <main>
+        <HeroSection />
+        <ContactBar />
+        <AboutMe />
+        <ExperienceSec />
+        <SoftSkills />
+        <LatestWork />
+        <EducationSkills />
+        <Contact />
+      </main>
+    </>
+  )
+}
+
+export default page
